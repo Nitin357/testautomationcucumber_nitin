fde143718dbab0f32036d89396923cd43fe8a680
Nmagar357 <92437216+Nmagar357@users.noreply.github.com>
2023-11-24 20:45:50 +0530

    updated cucumber framework

---
+++ src/test/java/stepDefinations/OrangeHRMSteps.java
@@ -0,0 +1,29 @@
+package stepDefinations;
+
+import io.cucumber.java.en.*;
+
+public class OrangeHRMSteps {
+
+    @Given("I launch Chrome browser")
+    public void i_launch_chrome_browser() {
+
+    }
+
+    @When("I open OrangeHRM Home Page")
+    public void i_open_orange_hrm_home_page() {
+
+    }
+
+    @Then("I verify OrangeHRM logo Present on Home Page")
+    public void i_verify_orange_hrm_logo_present_on_home_page() {
+
+    }
+
+    @Then("close Browser")
+    public void close_browser() {
+        
+    }
+
+
+
+}
